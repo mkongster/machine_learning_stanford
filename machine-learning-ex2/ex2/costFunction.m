3ecfa4e845ac662ccbc50169eedfe75875d07494
Ming Kong <mkongg92@gmail.com>
2020-05-19 19:08:02 -0400

    started exercise 2, completed sigmoid, plotData, costFunction

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,7 +20,12 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X * theta);
+J = (1/m) * ((-y' * log(h)) - ((1-y)' * log(1 - h)));
 
+# why not theta - 1/m * X' * (h-y)
+#   only want gradient, not gradient descent?
+grad = 1/m * X' * (h-y);
 
 
 
